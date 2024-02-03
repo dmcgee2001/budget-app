5312e3c539a9c54b92dc5263d94f98691d3ed2c3
Derek McGee <dmcgee2001@gmail.com>
2024-02-03 15:37:29 -0500

    uses strong params in user controller rather than defined in create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
   def create
-    user = User.new(
-      name: params[:name],
-      email: params[:email],
-      password: params[:password],
-      password_confirmation: params[:password_confirmation],
-    )
+    user = User.new(user_params)
     if user.save
       render json: { message: "User created successfully" }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :name, :email, :password, :password_confirmation
+    )
   end
 end
