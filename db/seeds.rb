98baa1c174d3d00f690a0a7d42a9d0802a41a791
Derek McGee <dmcgee2001@gmail.com>
2024-02-18 13:06:08 -0500

    adds seeds for data testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+VariableExpense.create!([
+  {groceries: "766.0", gas: "184.0", eating_out: "67.0", personal_care: "54.0", utilities: "259.0", entertainment: "95.0", auto_maintenance: "175.0", property_maintenance: "95.0", other: "437.0"},
+  {groceries: "385.0", gas: "143.0", eating_out: "51.0", personal_care: "41.0", utilities: "219.0", entertainment: "93.0", auto_maintenance: "155.0", property_maintenance: "151.0", other: "255.0"},
+  {groceries: "531.0", gas: "161.0", eating_out: "189.0", personal_care: "24.0", utilities: "395.0", entertainment: "154.0", auto_maintenance: "190.0", property_maintenance: "74.0", other: "162.0"},
+  {groceries: "744.0", gas: "94.0", eating_out: "193.0", personal_care: "28.0", utilities: "222.0", entertainment: "107.0", auto_maintenance: "70.0", property_maintenance: "110.0", other: "454.0"},
+  {groceries: "706.0", gas: "151.0", eating_out: "117.0", personal_care: "62.0", utilities: "253.0", entertainment: "115.0", auto_maintenance: "107.0", property_maintenance: "174.0", other: "94.0"},
+  {groceries: "226.0", gas: "180.0", eating_out: "143.0", personal_care: "98.0", utilities: "316.0", entertainment: "62.0", auto_maintenance: "146.0", property_maintenance: "105.0", other: "201.0"},
+  {groceries: "713.0", gas: "85.0", eating_out: "171.0", personal_care: "60.0", utilities: "378.0", entertainment: "134.0", auto_maintenance: "151.0", property_maintenance: "188.0", other: "346.0"},
+  {groceries: "751.0", gas: "67.0", eating_out: "186.0", personal_care: "29.0", utilities: "333.0", entertainment: "73.0", auto_maintenance: "86.0", property_maintenance: "193.0", other: "467.0"},
+  {groceries: "206.0", gas: "132.0", eating_out: "57.0", personal_care: "22.0", utilities: "384.0", entertainment: "200.0", auto_maintenance: "106.0", property_maintenance: "188.0", other: "425.0"},
+  {groceries: "748.0", gas: "132.0", eating_out: "68.0", personal_care: "79.0", utilities: "364.0", entertainment: "153.0", auto_maintenance: "142.0", property_maintenance: "180.0", other: "481.0"}
+])
+Income.create!([
+  {monthly_salary: "6643.0", biweekly_salary: "2980.0", weekly_salary: "988.0", rental_properties: "859.0", car_rental: "619.0", item_sales: "499.0", other: "658.0"},
+  {monthly_salary: "5578.0", biweekly_salary: "3893.0", weekly_salary: "1466.0", rental_properties: "963.0", car_rental: "889.0", item_sales: "477.0", other: "347.0"},
+  {monthly_salary: "7543.0", biweekly_salary: "4076.0", weekly_salary: "931.0", rental_properties: "1913.0", car_rental: "467.0", item_sales: "877.0", other: "391.0"},
+  {monthly_salary: "8592.0", biweekly_salary: "2594.0", weekly_salary: "790.0", rental_properties: "1454.0", car_rental: "318.0", item_sales: "658.0", other: "872.0"},
+  {monthly_salary: "3984.0", biweekly_salary: "3381.0", weekly_salary: "1030.0", rental_properties: "761.0", car_rental: "219.0", item_sales: "235.0", other: "646.0"},
+  {monthly_salary: "5421.0", biweekly_salary: "2483.0", weekly_salary: "1002.0", rental_properties: "1475.0", car_rental: "740.0", item_sales: "646.0", other: "158.0"},
+  {monthly_salary: "4999.0", biweekly_salary: "4646.0", weekly_salary: "2340.0", rental_properties: "1821.0", car_rental: "348.0", item_sales: "242.0", other: "321.0"},
+  {monthly_salary: "7475.0", biweekly_salary: "3788.0", weekly_salary: "2139.0", rental_properties: "931.0", car_rental: "304.0", item_sales: "155.0", other: "879.0"},
+  {monthly_salary: "3941.0", biweekly_salary: "1735.0", weekly_salary: "1912.0", rental_properties: "1033.0", car_rental: "649.0", item_sales: "330.0", other: "992.0"},
+  {monthly_salary: "7051.0", biweekly_salary: "3902.0", weekly_salary: "1383.0", rental_properties: "1378.0", car_rental: "713.0", item_sales: "557.0", other: "216.0"}
+])
+FixedExpense.create!([
+  {mortgage: "2397.0", car_payment: "143.0", auto_insurance: "120.0", health_insurance: "139.0", life_insurance: "32.0", home_insurance: "92.0", renters_insurance: "63.0", dental_insurance: "59.0", other_insurance: "60.0", subscriptions: "37.0", student_loans: "950.0", cell_phone: "85.0", internet: "56.0", other: "54.0"},
+  {mortgage: "2359.0", car_payment: "434.0", auto_insurance: "154.0", health_insurance: "68.0", life_insurance: "32.0", home_insurance: "73.0", renters_insurance: "44.0", dental_insurance: "73.0", other_insurance: "82.0", subscriptions: "99.0", student_loans: "890.0", cell_phone: "35.0", internet: "69.0", other: "462.0"},
+  {mortgage: "4436.0", car_payment: "333.0", auto_insurance: "104.0", health_insurance: "140.0", life_insurance: "73.0", home_insurance: "64.0", renters_insurance: "38.0", dental_insurance: "41.0", other_insurance: "90.0", subscriptions: "52.0", student_loans: "734.0", cell_phone: "76.0", internet: "99.0", other: "228.0"},
+  {mortgage: "2074.0", car_payment: "144.0", auto_insurance: "180.0", health_insurance: "75.0", life_insurance: "67.0", home_insurance: "53.0", renters_insurance: "70.0", dental_insurance: "59.0", other_insurance: "25.0", subscriptions: "70.0", student_loans: "368.0", cell_phone: "84.0", internet: "44.0", other: "385.0"},
+  {mortgage: "2682.0", car_payment: "211.0", auto_insurance: "50.0", health_insurance: "141.0", life_insurance: "57.0", home_insurance: "64.0", renters_insurance: "35.0", dental_insurance: "48.0", other_insurance: "47.0", subscriptions: "60.0", student_loans: "609.0", cell_phone: "69.0", internet: "75.0", other: "254.0"},
+  {mortgage: "4564.0", car_payment: "197.0", auto_insurance: "127.0", health_insurance: "106.0", life_insurance: "78.0", home_insurance: "88.0", renters_insurance: "70.0", dental_insurance: "20.0", other_insurance: "33.0", subscriptions: "67.0", student_loans: "904.0", cell_phone: "36.0", internet: "39.0", other: "465.0"},
+  {mortgage: "3651.0", car_payment: "228.0", auto_insurance: "139.0", health_insurance: "190.0", life_insurance: "69.0", home_insurance: "51.0", renters_insurance: "80.0", dental_insurance: "41.0", other_insurance: "28.0", subscriptions: "56.0", student_loans: "877.0", cell_phone: "20.0", internet: "32.0", other: "428.0"},
+  {mortgage: "2591.0", car_payment: "495.0", auto_insurance: "159.0", health_insurance: "141.0", life_insurance: "52.0", home_insurance: "91.0", renters_insurance: "28.0", dental_insurance: "73.0", other_insurance: "23.0", subscriptions: "69.0", student_loans: "876.0", cell_phone: "29.0", internet: "86.0", other: "478.0"},
+  {mortgage: "1745.0", car_payment: "383.0", auto_insurance: "116.0", health_insurance: "79.0", life_insurance: "29.0", home_insurance: "84.0", renters_insurance: "74.0", dental_insurance: "37.0", other_insurance: "70.0", subscriptions: "50.0", student_loans: "371.0", cell_phone: "77.0", internet: "51.0", other: "468.0"},
+  {mortgage: "3536.0", car_payment: "415.0", auto_insurance: "174.0", health_insurance: "128.0", life_insurance: "23.0", home_insurance: "83.0", renters_insurance: "32.0", dental_insurance: "67.0", other_insurance: "69.0", subscriptions: "48.0", student_loans: "497.0", cell_phone: "65.0", internet: "88.0", other: "311.0"}
+])
